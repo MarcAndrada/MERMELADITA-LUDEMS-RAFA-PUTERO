8e73e0bd65c22a3d5fc282c420e1061bd591efa4
JUDITH ESPÍGOL MIGUEL <judith.espigol@enti.cat>
2023-10-01 14:21:52 +0200

    عاهرة ميفي

--- Assets/Scripts/CanonController.cs
+++ Assets/Scripts/CanonController.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class CanonController : MonoBehaviour
 {
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private GameObject map;
+    [SerializeField]
+    private GameObject player;
+
+
+    [SerializeField]
+    private float speed; 
+
+    Rigidbody2D rb2d;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
     }
-   
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InstantitateEnemies();
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 point = map.transform.position;
+        Vector3 axis = new Vector3(0, 0, 1);
+        transform.RotateAround(point, axis, Time.deltaTime * speed);
+    }
+
+    private void InstantitateEnemies()
+    {
+
+        BallController currentBall = Instantiate(ball, transform.position, Quaternion.identity).GetComponent<BallController>();
+        currentBall.SetPlayerPos(player.transform.position);
     }
 }
